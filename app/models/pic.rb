3ef5b8e5ce0adc289d4d9ac8f86f1b52796d2c1d
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 03:23:32 +0900

    use reduced redundancy https://github.com/marcel/aws-s3/commit/1ec3ff9d39dc3dd7ec5ea68278b9fbd1dbe87a88 https://github.com/marcel/aws-s3/pull/44

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -39,6 +39,7 @@ class Pic < ActiveRecord::Base
     AWS::S3::S3Object.store(s3_name, open(zip), 'picplz-zip-de-kure', {
         :content_type => 'application/zip',
         :access => :public_read,
+        'x-amz-storage-class' => 'REDUCED_REDUNDANCY',
       })
     public_url = AWS::S3::S3Object.url_for(s3_name, 'picplz-zip-de-kure', { :authenticated => false })
     Rails.logger.info("stored to #{public_url}")
