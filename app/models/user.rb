135006379551ecdfa91ee02639da8facf50da5f5
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 05:51:50 +0900

    uid string

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,7 @@ class User < ActiveRecord::Base
   def self.create_with_omniauth(auth)
     create! do |user|
       user.provider = auth['provider']
-      user.uid      = auth['uid']
+      user.uid      = auth['uid'].to_s
       user.nickname = auth['info']['nickname']
       user.url      = auth['info']['urls']['Picplz']
       user.image    = auth['info']['image']
