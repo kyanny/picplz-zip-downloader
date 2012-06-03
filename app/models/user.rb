4bddf6a557b9128d0894e16817e3b807e6870779
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 00:58:35 +0900

    get and save token

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,15 @@ class User < ActiveRecord::Base
       user.nickname = auth['info']['nickname']
       user.url      = auth['info']['urls']['Picplz']
       user.image    = auth['info']['image']
-      user.token    = auth['credentials']['token']
-      user.secret   = auth['credentials']['secret']
+      user.token    = token_and_secret_from_auth_hash(auth)['oauth_token']
+      user.secret   = token_and_secret_from_auth_hash(auth)['oauth_secret']
     end
   end
+
+  def self.token_and_secret_from_auth_hash(auth)
+    Hash[*auth['credentials']['token'].split('|').last.split('&').map{ |keyvalue| keyvalue.split('=') }.flatten]
+  end
 end
+
+
+
