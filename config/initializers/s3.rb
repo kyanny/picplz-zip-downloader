fe3946e4a797ad7dfc45e9a544a191a418c1ad87
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 02:59:50 +0900

    create bucket when initialize

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -2,3 +2,14 @@ AWS::S3::Base.establish_connection!(
   :access_key_id     => ENV['ACCESS_KEY_ID'],
   :secret_access_key => ENV['SECRET_ACCESS_KEY'],
   )
+AWS::S3::Bucket.create('picplz-zip-de-kure')
+
+
+
+
+
+
+
+
+
+
