91865f57cb63df9a40f409b87cb005ed6fcca1cf
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 06:21:36 +0900

    Revert "connect to s3 (initializer cannot read heroku:config)"
    This reverts commit 19c3b8d235e8f7e05a436b27a82f1317e77759bb.

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,1 +1,6 @@
+AWS::S3::Base.establish_connection!(
+  :access_key_id     => ENV['ACCESS_KEY_ID'],
+  :secret_access_key => ENV['SECRET_ACCESS_KEY'],
+  )
 PicplzZipDeKure::Application.config.s3_bucket_name = 'picplz-zip-de-kure'
+AWS::S3::Bucket.create(PicplzZipDeKure::Application.config.s3_bucket_name)
