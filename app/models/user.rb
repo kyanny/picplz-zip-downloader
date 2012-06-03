2196b69fad36f199ffeaad5a2891abf07586bf93
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 01:19:58 +0900

    batch (recursive) pic api call method

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,25 @@ class User < ActiveRecord::Base
   def self.token_and_secret_from_auth_hash(auth)
     Hash[*auth['credentials']['token'].split('|').last.split('&').map{ |keyvalue| keyvalue.split('=') }.flatten]
   end
+
+  def get_pics_from_picplz(last_pic_id=nil)
+    require 'open-uri'
+    url = "http://api.picplz.com/api/v2/user.json?id=#{self.uid}&include_pics=1&oauth_token=#{self.token}"
+    if last_pic_id
+      url = "#{url}&last_pic_id=#{last_pic_id}"
+    end
+
+    open(url){ |f|
+      res = JSON.parse(f.read)
+      res['value']['users'][0]['pics'].each_with_index do |pic, index|
+        # warn pic['pic_files']['640r']['img_url']
+        #warn "#{index}|#{pic['pic_files']['640r']['img_url']}"
+      end
+
+      if res['value']['users'][0]['more_pics']
+        last_pic_id = res['value']['users'][0]['last_pic_id']
+        self.get_pics_from_picplz(last_pic_id)
+      end
+    }
+  end
 end
-
-
-
