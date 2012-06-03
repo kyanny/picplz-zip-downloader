1380d247201ea262e3f640e0b0f4c02621c797cf
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 01:28:38 +0900

    download method

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,4 +1,14 @@
 class Pic < ActiveRecord::Base
   attr_accessible :archived, :downloaded, :url, :user_id
   belongs_to :user
+
+  def download
+    open(self.url){ |image|
+      workdir = Rails.root.join('tmp', user.uid)
+      FileUtils.mkdir_p(workdir) unless Dir.exists?(workdir)
+      open(workdir.join(self.id, 'w'){ |f|
+          f.write image.read
+        }
+      }
+  end
 end
