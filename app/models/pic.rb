6bca506658e4a5691385e969ae9c709f948d5fdf
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 01:57:11 +0900

    download with delayed_job

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -3,11 +3,14 @@ class Pic < ActiveRecord::Base
   belongs_to :user
 
   def download
+    return if self.downloaded
     open(self.url){ |image|
       workdir = Rails.root.join('tmp', user.uid.to_s)
       FileUtils.mkdir_p(workdir) unless Dir.exists?(workdir)
-      open(workdir.join(self.pic_id.to_s), 'w'){ |f|
+      file = workdir.join(self.pic_id.to_s).to_s + '.jpg'
+      open(file, 'wb'){ |f|
         f.write image.read
+        Rails.logger.info("download from #{self.url}")
       }
     }
     self.downloaded = true
