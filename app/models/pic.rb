8b2f0062c1e3e8b2c82d73ecd39a25c8c25627ba
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 02:38:01 +0900

    human-readable? filename

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -2,12 +2,16 @@ class Pic < ActiveRecord::Base
   attr_accessible :archived, :downloaded, :url, :user_id
   belongs_to :user
 
+  def longurl_id
+    url.split('/')[-2]
+  end
+
   def download
     return if self.downloaded
     open(self.url){ |image|
       workdir = Rails.root.join('tmp', user.nickname)
       FileUtils.mkdir_p(workdir) unless Dir.exists?(workdir)
-      file = workdir.join(self.pic_id.to_s).to_s + '.jpg'
+      file = workdir.join(self.longurl_id).to_s + '.jpg'
       open(file, 'wb'){ |f|
         f.write image.read
         Rails.logger.info("download from #{self.url}")
