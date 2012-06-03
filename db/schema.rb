e9f185f4bdf3946a342b61010324e9a645999fdd
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 03:26:13 +0900

    migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120603162349) do
+ActiveRecord::Schema.define(:version => 20120603182500) do
+
+  create_table "archives", :force => true do |t|
+    t.integer  "user_id"
+    t.boolean  "downloaded"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
