5e3235fd122dabba70752c55d8ea16c88a257d91
Kensuke Nagae <kyanny@gmail.com>
2012-06-03 23:47:30 +0900

    toriaezu commit but not work :(

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,64 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+// http://picplz.com/user/kyanny/pic/105sg/download/
+// text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8
+
+function download(uid, last_pic_id){
+  var data = {
+    id: uid,
+    include_pics: 1
+  };
+  if (last_pic_id) {
+    data.last_pic_id = last_pic_id;
+  }
+  
+  $.getJSON('http://api.picplz.com/api/v2/user.json?callback=?', data, function(res){
+    $.each(res.value.users[0].pics, function(index, pic){
+      var download_url = 'http://picplz.com' + pic.url + 'download/';
+      embed(download_url);
+    });
+    
+    if (res.value.users[0].more_pics) {
+      var last_pic_id = res.value.users[0].last_pic_id;
+      download(uid, last_pic_id);
+    } else {
+      done();
+    }
+  });
+}
+
+function embed(url){
+  $('<li>').append(
+    $('<a>').attr({
+      href: url,
+      target: '_blank'
+    }).text(url)
+  ).appendTo('#pics ul');
+}
+
+function done(){
+  $('#loading p').text('done!');
+  $('#download').show();
+}
+  
+$(function(){
+  $('#download').click(function(){
+    var pic_count = $('#pics a').length;
+    Deferred.loop({begin:1, end:pic_count, step:10}, function(n, o){
+      console.log(n);
+      
+      return o.last? n : Deferred.wait(5);
+    });
+//    alert($('#pics a').length);
+//    $('#pics a').each(function(index, elem){
+//      elem.click();
+//    });
+  });
+  
+  var uid = $('#uid').val();
+  if (uid) {
+    download(uid, null);
+  }
+});
--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,18 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 0) do
+ActiveRecord::Schema.define(:version => 20120603113605) do
+
+  create_table "users", :force => true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "nickname"
+    t.string   "url"
+    t.string   "image"
+    t.string   "token"
+    t.string   "secret"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
 end
