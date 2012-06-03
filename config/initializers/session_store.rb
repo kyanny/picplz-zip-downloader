4b53da761fe81999974a4f313250cfd6d0c15692
Kensuke Nagae <kyanny@gmail.com>
2012-06-03 20:16:01 +0900

    rails new

---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+PicplzZipDeKure::Application.config.session_store :cookie_store, key: '_picplz-zip-de-kure_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# PicplzZipDeKure::Application.config.session_store :active_record_store
