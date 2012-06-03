f28c3a557fa26c8a686cca0823d09368b7257f22
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 01:37:34 +0900

    fix to_s

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -4,9 +4,9 @@ class Pic < ActiveRecord::Base
 
   def download
     open(self.url){ |image|
-      workdir = Rails.root.join('tmp', user.uid)
+      workdir = Rails.root.join('tmp', user.uid.to_s)
       FileUtils.mkdir_p(workdir) unless Dir.exists?(workdir)
-      open(workdir.join(self.id), 'w'){ |f|
+      open(workdir.join(self.pic_id.to_s), 'w'){ |f|
         f.write image.read
       }
     }
