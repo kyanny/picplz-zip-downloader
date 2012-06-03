adc98a9a363bdb6d2a340019e3104117a75623f2
Kensuke Nagae <kyanny@gmail.com>
2012-06-04 01:24:27 +0900

    assoc

--- app/models/pic.rb
+++ app/models/pic.rb
@@ -1,3 +1,4 @@
 class Pic < ActiveRecord::Base
   attr_accessible :archived, :downloaded, :url, :user_id
+  belongs_to :user
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   attr_accessible :name, :provider, :uid
+  has_many: pics
 
   def self.create_with_omniauth(auth)
     create! do |user|
